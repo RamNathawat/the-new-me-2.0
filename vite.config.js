fff12e8c4a67f8a2ad3c2907f629fd16e82cec06
Ram nathawat <nathawatr7@gmail.com>
2026-05-29 11:09:04 +0530

    refinement performance

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,17 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     entries: ['index.html']
+  },
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor_react: ['react', 'react-dom'],
+          vendor_three: ['three', '@react-three/fiber', '@react-three/drei'],
+          vendor_gsap: ['gsap']
+        }
+      }
+    }
   }
 })
